07c9da73642518c8b9fed4d18714f049f6f86ff4
Alexey Mileev <alemileev@gmail.com>
2017-08-25 17:01:27 -0700

    Add /notes PUT request

--- routes/notes.js
+++ routes/notes.js
@@ -1,23 +1,43 @@
+"use strict";
+
 const express = require('express');
 const router = express.Router();
-const {Pool, Client} = require('pg');
+const {Pool, Query} = require('pg');
 
 router.put('/', (request, response) => {
     const text = request.body.text;
-    // const client = new Client();
-    // client.connect();
-    // client.end();
-    // res.send({id: text})
     const pool = new Pool();
-    pool.query('SELECT NOW() as tm', (err, res) => {
-        response.send(`text: ${text}, err: ${err}, res: ${res}`);
-        pool.end();
+    pool.on('error', (err) => {
+        console.error('Unexpected error on idle client', err);
+        process.exit(-1);
     });
+    pool.connect((err, client, done) => {
+        if (err) {
+            throw err;
+        }
+        const query = client.query(new Query(
+            'INSERT INTO notes (text) VALUES ($1) RETURNING id',
+            [text]
+        ));
+        query.on('row', (row) => {
+            console.log(row);
+            response.send({
+                id: row.id,
+                url: request.headers.host + '/notes/' + row.id
+            })
+        });
+        query.on('error', (err) => {
+            console.log(err);
+            response.error(err);
+        });
+        done();
+    });
+    pool.end();
 });
 
-router.get('/', (req, res) => {
+router.get('/', (request, response) => {
     const test = process.env.TEST || null;
-    res.send(`respond with a user; TEST=${test}`);
+    response.send(`respond with a user; TEST=${test}`);
 });
 
 module.exports = router;
